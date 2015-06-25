8e7f6e69501728316161a7471ddafb199dd752ca
Robin Edman <robin.edman@gmail.com>
2015-06-25 12:05:00 +0100

    Added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+loans = [
+  {
+    provider: 'easycredit',
+    min_loan: 1000,
+    max_loan: 5000
+  },
+  {
+    provider: 'expresskredit',
+    min_loan: 500,
+    max_loan: 8000
+  }
+]
+
+loans.each { |loan| Loan.create(loan) }
